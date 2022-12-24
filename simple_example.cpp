e13d4d488fc0eeaaa628f2885016df55b7f4f029
Andrcraft9 <achaplygin99@gmail.com>
2022-12-24 21:12:01 +0300

    Init

---
+++ simple_example.cpp
@@ -0,0 +1,16 @@
+#include "simple_lib.hpp"
+
+#include <iostream>
+
+int main() {
+  core::HelloTriangleApplication app;
+
+  try {
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
